7054f44731f552075b819a322a5f621fbdc7c431
Moid Malik <abdulmoid2007@gmail.com>
2025-01-31 13:50:37 +0500

    remove bad code

--- components/navbar/search.tsx
+++ components/navbar/search.tsx
@@ -1,37 +1,39 @@
-'use client'
+"use client";
 
-import { useRouter } from 'next/navigation'
-import { Input } from '../ui/input'
-import { useDebouncedCallback } from 'use-debounce'
+import { useRouter } from "next/navigation";
+import { Input } from "../ui/input";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search() {
-    const router = useRouter()
+  const router = useRouter();
 
-    const handleSearch = useDebouncedCallback((term: string) => {
-        const currentUrl = new URL(window.location.href)
-        if (term) {
-            currentUrl.searchParams.set('search', term)
-        } else {
-            currentUrl.searchParams.delete('search')
-        }
-        router.push(currentUrl.pathname + currentUrl.search, { scroll: false })
-    }, 50)
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams();
 
-    const getCurrentSearchTerm = () => {
-        if (typeof window !== 'undefined') {
-            return new URLSearchParams(window.location.search).get('search') || ''
-        }
-        return ''
+    if (term) {
+      params.set("search", term);
     }
 
-    return (
-        <Input 
-            type='search'
-            name='search'
-            placeholder='Search...'
-            className='w-[400px]'
-            onChange={(e) => handleSearch(e.target.value)}
-            defaultValue={getCurrentSearchTerm()}
-        />
-    )
+    router.push(`/catalog${params.toString() ? `?${params.toString()}` : ""}`, {
+      scroll: false,
+    });
+  }, 50);
+
+  const getCurrentSearchTerm = () => {
+    if (typeof window !== "undefined") {
+      return new URLSearchParams(window.location.search).get("search") || "";
+    }
+    return "";
+  };
+
+  return (
+    <Input
+      type="search"
+      name="search"
+      placeholder="Search..."
+      className="w-[400px]"
+      onChange={(e) => handleSearch(e.target.value)}
+      defaultValue={getCurrentSearchTerm()}
+    />
+  );
 }
